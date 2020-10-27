5bf5d5b3e5cd559ce159eec8fb8480daf5ed2df3
Marcin Mitura <mitura07@wp.pl>
2020-10-27 10:49:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo("<h3>Numer 22</h3>");
 $servername="localhost";
 $username="marcin";
 $password="2407";
-$dbname="Nauka_SDB";
+$dbname="Nauka_SBD";
 
 //Zadanie 1: Wyświetlenie pełnej listy pracowników:
 echo("<h1>Zadanie 1: Wyświetlenie pełnej listy pracowników:</h1>");
@@ -31,14 +31,14 @@ echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
 
 //Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:
 echo("<h1>Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:</h1>");
-$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SDB");
+$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SBD");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a'");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -48,14 +48,14 @@ echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
 
 //Zadanie 3: Kobiety z działu 1 i 4:
 echo("<h1>Zadanie 3: Kobiety z działu 1 i 4:</h1>");
-$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SDB");
+$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SBD");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND dzial=1 OR dzial=4");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -65,14 +65,14 @@ echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
 
 //Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:
 echo("<h1>Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:</h1>");
-$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SDB");
+$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SBD");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND (zarobki<35) AND (dzial=1 OR dzial=2)");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -82,14 +82,14 @@ echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
 
 //Zadanie 5: Mężczyźni zarabiający mniej niż 30:
 echo("<h1>Zadanie 5: Mężczyźni zarabiający mniej niż 30:</h1>");
-$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SDB");
+$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SBD");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie NOT LIKE '%a' AND (zarobki<30)");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -99,14 +99,14 @@ echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
 
 //Zadanie 6: Wyświetlenie "MOJA_SUMA" (funkcja SUM)
 echo("<h1>Zadanie 6: Wyświetlenie 'MOJA_SUMA' (funkcja SUM)</h1>");
-$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SDB");
+$conn=new mysqli("localhost", "marcin", "2407", "Nauka_SBD");
 $result=$conn->query("SELECT dzial, SUM(zarobki) AS MOJA_SUMA FROM pracownicy GROUP BY dzial");
 echo("<table borded=1>");
 echo("<th>dzial</th>");
